3dd17c9991a3e2e5707fb140874051e585725daa
Hector Gameros <third.loosest0e@icloud.com>
2025-02-25 13:21:05 -0700

    Chapter 10
    Extend data fetching delay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -33,7 +33,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     await new Promise((resolve) => {
-      setTimeout(resolve, 4000);
+      setTimeout(resolve, 10000);
     });
 
     const data = await sql<LatestInvoiceRaw[]>`
@@ -57,7 +57,7 @@ export async function fetchLatestInvoices() {
 export async function fetchCardData() {
   try {
     await new Promise((resolve) => {
-      setTimeout(resolve, 2000);
+      setTimeout(resolve, 15000);
     });
 
     // You can probably combine these into a single SQL query
